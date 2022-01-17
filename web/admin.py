9d535cad6ddc8ab2b58b8d41c4d268a3f6702aa8
Dehghani <amin.dehgan5656@gmail.com>
2022-01-17 23:33:26 +0330

    [ADD] Product creation

--- web/admin.py
+++ web/admin.py
@@ -41,15 +41,47 @@ def login():
 
 @app.route('/admin/dashboard')
 def dashboard():
-    if not session.get('uid') is None:
-        return render_template('admin/dashboard.html')
-    else:
+    if session.get('uid') is None:
         return redirect(url_for('login'))
+    return render_template('admin/dashboard.html')
 
 
 @app.route('/admin/products')
 def products():
-    if not session.get('uid') is None:
-        return render_template('admin/products.html')
-    else:
+    if session.get('uid') is None:
         return redirect(url_for('login'))
+    values = {}
+    cursor = app.config['db'].cursor()
+    cursor.execute("SELECT * FROM Product")
+    products = cursor.fetchall()
+    values.update({
+        'products': products
+    })
+    return render_template('admin/products.html', values=values)
+
+
+@app.route('/admin/products/add', methods=['GET', 'POST'])
+def add_product():
+    if session.get('uid') is None:
+        return redirect(url_for('login'))
+    values = {}
+    if request.method == 'POST':
+        missing = []
+        form_data = request.form
+        if not form_data.get('name') or form_data.get('name') == '':
+            missing.append('Product Name')
+        if not form_data.get('price') or form_data.get('price') == '':
+            missing.append('Product Price')
+        if not form_data.get('description') or form_data.get('description') == '':
+            missing.append('Product Description')
+        if not missing:
+            try:
+                cursor = app.config['db'].cursor()
+                query = "INSERT INTO Product (name, price, description) VALUES (\"%s\", %s, \"%s\")" % (form_data.get('name'), form_data.get('price'), form_data.get('description'))
+                cursor.execute(query)
+                app.config['db'].commit()
+                return redirect(url_for('products'))
+            except Exception as e:
+                print(e)
+                values.update({'error': e})
+    return render_template('admin/add_product.html')
